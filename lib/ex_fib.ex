3de08d84514e5c4d6bf997711e8526854fd6a83d
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-10-14 07:43:49 +0900

    add a generator of fibonacci sequence

--- lib/ex_fib.ex
+++ lib/ex_fib.ex
@@ -1,18 +1,34 @@
 defmodule ExFib do
   @moduledoc """
-  Documentation for ExFib.
+  Enumerable Fibonacci numbers.
   """
 
-  @doc """
-  Hello world.
+  defstruct f1: 1, f2: 1
 
-  ## Examples
+  def value(%ExFib{f1: f1}) do
+    f1
+  end
 
-      iex> ExFib.hello()
-      :world
+  def succ(%ExFib{f1: f1, f2: f2}) do
+    %ExFib{f1: f2, f2: f1 + f2}
+  end
 
-  """
-  def hello do
-    :world
+  defimpl Enumerable do
+    def count(%ExFib{}), do: {:error, __MODULE__}
+
+    def slice(%ExFib{}), do: {:error, __MODULE__}
+
+    def member?(%ExFib{f1: value}, value), do: {:ok, true}
+
+    def member?(%ExFib{f1: f1, f2: f2}, value) when f1 < value,
+      do: member?(%ExFib{f1: f2, f2: f1 + f2}, value)
+
+    def member?(%ExFib{}, _), do: {:ok, false}
+
+    def reduce(%ExFib{}, {:halt, acc}, _), do: {:halted, acc}
+    def reduce(%ExFib{} = fib, {:suspend, acc}, fun), do: {:suspended, acc, &reduce(fib, &1, fun)}
+
+    def reduce(%ExFib{f1: f1} = fib, {:cont, acc}, fun),
+      do: reduce(ExFib.succ(fib), fun.(f1, acc), fun)
   end
 end
--- test/ex_fib_test.exs
+++ test/ex_fib_test.exs
@@ -2,7 +2,44 @@ defmodule ExFibTest do
   use ExUnit.Case
   doctest ExFib
 
-  test "greets the world" do
-    assert ExFib.hello() == :world
+  describe "member?" do
+    test "55 exists within Fibonacci sequence" do
+      assert Enum.member?(%ExFib{}, 55)
+    end
+
+    test "354224848179261915075 (= f(100)) exists within Fibonacci sequence" do
+      assert Enum.member?(%ExFib{}, 354_224_848_179_261_915_075)
+    end
+
+    test "550 doesn't exist within Fibonacci sequence" do
+      refute Enum.member?(%ExFib{}, 550)
+    end
+
+    test "3542248481792619150750 (= f(100) * 10) doesn't exist within Fibonacci sequence" do
+      refute Enum.member?(%ExFib{}, 3_542_248_481_792_619_150_750)
+    end
+  end
+
+  describe "reduce" do
+    setup do
+      [expects: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]]
+    end
+
+    test "take", %{expects: expects} do
+      fibs =
+        %ExFib{}
+        |> Enum.take(10)
+
+      assert fibs == expects
+    end
+
+    test "zip", %{expects: expects} do
+      comparisons =
+        %ExFib{}
+        |> Enum.zip(expects)
+        |> Enum.map(fn {actual, expected} -> actual == expected end)
+
+      assert Enum.all?(comparisons)
+    end
   end
 end
